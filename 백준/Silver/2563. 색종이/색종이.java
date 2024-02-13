8d846a9d778caffcb8526165d7d292734b4cc581
jang-guan-hoho <156163376+jang-guan-hoho@users.noreply.github.com>
2024-02-13 16:46:52 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,27 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map = new int [100][100];
-	public static void paper(int x,int y) {
-		for(int r=x;r<x+10;r++) {
-			for(int c=y;c<y+10;c++) {
-				map[r][c] = 1;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int[][] arr = new int[100][100];
+
+		int N = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int X = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			for (int x = 0; x < 10; x++) {
+				for (int y = 0; y < 10; y++) {
+					arr[X + x - 1][Y + y - 1] += 1;
+				}
 			}
 		}
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		int cnt=0;
-		for(int i=0;i<t;i++) {
-			paper(sc.nextInt(), sc.nextInt());
-		}
-		for(int r=0;r<100;r++) {
-			for(int c=0;c<100;c++) {
-				if(map[r][c]==1)
-					cnt++;
+
+		int subtract = 0;
+		for (int x = 0; x < 100; x++) {
+			for (int y = 0; y < 100; y++) {
+				if (arr[x][y] > 1) {
+					subtract += arr[x][y] - 1;
+				}
 			}
 		}
-		System.out.println(cnt);
+		System.out.println(100 * N - subtract);
+
 	}
 }
