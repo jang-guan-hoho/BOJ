5dcba1904282fff0c47c62ea863cc960fa70e6cc
jang-guan-hoho <156163376+jang-guan-hoho@users.noreply.github.com>
2024-02-26 16:39:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,20 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int ans = 0;
-		int cnt = Integer.MAX_VALUE;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				if(j*5+i*3==n&&j+i<cnt) {
-					cnt = j+i;
-					ans = cnt;
-				}
-			}
-		}
-		if(ans==0) ans--;
-		System.out.println(ans);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+        int N = Integer.parseInt(br.readLine());
+       
+        int countT = 0;
+        int countF = 0;
+        int extra = 0;
+        
+        countF = N/5;
+        extra = N%5;
+        countT = extra/3;
+        extra = extra%3;
+        int totalCount = Integer.MAX_VALUE;
+        boolean isTrue = false;
+        if(extra==0) {
+        	isTrue = true;
+        	totalCount = countF+countT;
+        }
+        
+        
+        for(int i = countF-1; i>=0; i--) {
+        	countF--;
+        	extra +=5;
+        	countT += extra/3;
+        	extra = extra%3;
+        	if(countF+countT<=totalCount && extra==0) {
+        		isTrue = true;
+        		totalCount = countF+countT;
+        	}
+        }
+        
+        if(!isTrue && extra != 0) {
+        	totalCount = -1;
+        }
+        
+        bw.write(totalCount+"");
+        bw.newLine();
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    
 	}
 }
