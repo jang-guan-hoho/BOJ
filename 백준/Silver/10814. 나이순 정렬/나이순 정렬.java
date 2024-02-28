96b5ca956a7297fff4fb1c16f8fc185a677cb57f
jang-guan-hoho <156163376+jang-guan-hoho@users.noreply.github.com>
2024-02-28 16:41:33 +0900

    [Silver V] Title: 나이순 정렬, Time: 1636 ms, Memory: 62060 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,32 +1,77 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	static int[] age;
+	static String[] name;
+	static int[] arr;
+	static String[] cname;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-	
-        List<String> members = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		age = new int [N];
+		name = new String [N];
+		arr = new int [N];
+		cname = new String[N];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			age[i] = Integer.parseInt(st.nextToken());
+			name[i] = st.nextToken();
+		}
+		
+		mergeSort(0, N-1);
+		for(int i=0;i<N;i++) {
+			System.out.println(age[i]+" "+name[i]);
+		}
+	}
 
-        for (int i = 0; i < N; i++) {
-            members.add(br.readLine());
-        }
+	static void mergeSort(int left, int right) {
+		if (left < right) {
 
-        members.sort(Comparator.comparingInt(s -> Integer.parseInt(s.split(" ")[0])));
+			int mid = (left + right) / 2;
+			mergeSort(left, mid);
+			mergeSort(mid + 1, right);
+			merge(left, mid, right);
+		}
+	}
 
-        for (String member : members) {
-            System.out.println(member);
-        }
-        
-        bw.flush();
-        bw.close();
-        br.close();
-    
+	private static void merge(int left, int mid, int right) {
+		int L = left;
+		int R = mid + 1;
+		int idx = left;
+
+		while (L <= mid && R <= right) {
+			if (age[L] <= age[R]) {
+				cname[idx] = name[L];
+				arr[idx++] = age[L++];
+			}
+			else {
+				cname[idx] = name[R];
+				arr[idx++] = age[R++];
+			}
+		}
+		// 위에 결과로 sortedArr에 마지막 인덱스를 제외하고 정렬됨
+		// 따라서 마지막 인덱스를 채워야한다.
+		if (L <= mid) {
+			for (int i = L; i <= mid; i++) {
+				cname[idx] = name[i];
+				arr[idx++] = age[i];
+			}
+		} else {
+			for (int i = R; i <= right; i++) {
+				cname[idx] = name[i];
+				arr[idx++] = age[i];
+			}
+		}
+		// 배열 갱신
+		for (int i = left; i <= right; i++) {
+			name[i] = cname[i];
+			age[i] = arr[i];
+		}
 	}
 }
